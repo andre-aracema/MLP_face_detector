fd51f2ae0a61615e0667bcc037b6bb0591c292c8
André Akira <andreakirapic@gmail.com>
2025-11-12 17:54:41 -0300

    adicionado Batch Norm, He Init, AdamW e Clipping

--- src/model_architecture.py
+++ src/model_architecture.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Input, Dropout
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, Input, Dropout, BatchNormalization, Activation
+from tensorflow.keras.optimizers import AdamW
 
 
 # Constrói a Arquitetura de um modelo MLP
@@ -10,8 +10,14 @@ def build_mlp_architecture(input_shape):
 
     model = Sequential([
         Input(shape=input_shape, name="input_layer"),
-        Dense(128, activation="relu", name="hidden_layer_1"),
-        Dense(64, activation="relu", name="hidden_layer_2"),
+        Dense(128, kernel_initializer='he_normal', name="hidden_layer_1"),
+        BatchNormalization(),
+        Activation('relu'),
+
+        Dense(64, kernel_initializer='he_normal', name="hidden_layer_2"),
+        BatchNormalization(),
+        Activation('relu'),
+
         Dense(1, activation="sigmoid", name="output_layer")
     ], name="MLP_simples")
 
@@ -24,10 +30,14 @@ def build_robust_mlp_architecture(input_shape):
     model = Sequential([
         Input(shape=input_shape, name="input_layer"),
         
-        Dense(256, activation="relu", name="hidden_layer_1"),
+        Dense(256, kernel_initializer='he_normal', name="hidden_layer_1"),
+        BatchNormalization(name="bn_1"),
+        Activation('relu', name="relu_1"),
         Dropout(0.4, name="dropout_1"), # Desliga 40% dos neurônios
         
-        Dense(128, activation="relu", name="hidden_layer_2"),
+        Dense(128, kernel_initializer='he_normal', name="hidden_layer_2"),
+        BatchNormalization(name="bn_2"),
+        Activation('relu', name="relu_2"),
         Dropout(0.4, name="dropout_2"), # Desliga 40% dos neurônios
         
         Dense(1, activation="sigmoid", name="output_layer")
@@ -36,11 +46,15 @@ def build_robust_mlp_architecture(input_shape):
     return model
 
 # Compila um modelo Keras, preparando-o para o treinamento
-def compile_model(model_to_compile, learning_rate):
-    print(f"Compilando modelo com taxa de aprendizado: {learning_rate}")
+def compile_model(model_to_compile, learning_rate, weight_decay=1e-4):
+    print(f"Compilando modelo com taxa de aprendizado: {learning_rate}, Weight Decay: {weight_decay}")
 
     model_to_compile.compile(
-        optimizer=Adam(learning_rate=learning_rate), 
+        optimizer=AdamW(
+            learning_rate=learning_rate, 
+            weight_decay=weight_decay,
+            clipnorm=1.0 
+        ), 
         loss='binary_crossentropy',                 
         metrics=['accuracy']
     )
